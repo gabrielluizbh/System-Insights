f0c3e98579e34251a8a4c27b62a5c310f3ac7f66
Gabriel Luiz <gabrielluiz@msn.com>
2019-10-07 19:27:05 -0300

    Add files via upload

---
+++ Script verifica em mais de um servidor se os recursos do System Insights (Informações do Sistema) estão desabilitados.ps1
@@ -0,0 +1,21 @@
+﻿## Script verifica em mais de um servidor se os recursos do System Insights (Informações do Sistema) estão desabilitados - Créditos Gabriel Luiz - www.gabrielluiz.com ##
+
+$Computers = @(“ADDS.contoso.local", “DHCP.contoso.local")
+$Statuses = foreach ($Computer in $Computers) {
+$Computer
+$DisabledCapabilities = (Get-InsightsCapability -ComputerName $Computer | where State -eq "Disabled").Name 
+foreach ($Capability in $DisabledCapabilities) {
+Get-InsightsCapabilityResult -Name $Capability -ComputerName $Computer | select *
+}
+}
+$Statuses
+
+# Explicação do comando.
+
+# O campo $Computers adicione os hostnames dos servidores que são verificados. 
+
+# Esse script apresenta todas as informações dos recursos desabilitados de dois servidores de uma só vez, demostrando o status, horário de agendamento e recurso do System Insights (Informações do Sistema).
+
+# Para melhor compreensão dos comandos apresentados acesse o link do artigo.
+
+# https://gabrielluizbh.hostingerapp.com/2019/10/system-insights
